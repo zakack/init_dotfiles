28ceab4ab3dfa421632f9e2bb14c944c912f05f1
Zak Kessler <zak@ack.rip>
2024-07-30 19:51:11 -0400

    cleaned up

--- archDependencies.sh
+++ archDependencies.sh
@@ -8,103 +8,91 @@ echo '#######################'
 echo 'installing yay'
 echo '#######################'
 if [ ! -x /usr/bin/yay ]; then
-	git clone https://aur.archlinux.org/yay.git
-	if [ -d "$(pwd)/yay" ]; then
-		cd yay
-		makepkg -si
-		cd ..
-		rm -rf yay
-	fi
+  git clone https://aur.archlinux.org/yay.git
+  if [ -d "$(pwd)/yay" ]; then
+    cd yay
+    makepkg -si
+    cd ..
+    rm -rf yay
+  fi
 else
-	echo 'nevermind, yay already installed...'
+  echo 'nevermind, yay already installed...'
 fi
 
 echo '#######################'
 echo 'installing base applications'
 echo '#######################'
 yay -S --noconfirm --needed \
-	fish \
-	alacritty \
-	pipewire-alsa \
-	octopi \
-	picom \
-	rofi \
-	dunst \
-	python-pywal \
-	polybar \
-	vim \
-	neovim \
-	neovim-lspconfig \
-	dfc \
-	fzf \
-	fd \
-	maim \
-	bat \
-	dust \
-	eza \
-	zoxide \
-	xdotool \
-	tmux \
-	unrar \
-	p7zip \
-	cronie \
-	rclone
+  fish \
+  alacritty \
+  pipewire-alsa \
+  octopi \
+  picom \
+  rofi \
+  dunst \
+  python-pywal \
+  polybar \
+  vim \
+  neovim \
+  neovim-lspconfig \
+  dfc \
+  fzf \
+  fd \
+  maim \
+  bat \
+  dust \
+  yazi \
+  eza \
+  zoxide \
+  xdotool \
+  tmux \
+  unrar \
+  p7zip \
+  cronie \
+  rclone
 
 #TODO: add nix support
 echo '#######################'
 echo 'installing development and util applications'
 echo '#######################'
 yay -S --noconfirm --needed \
-	nodejs \
-	npm \
-	python-pip \
-	python-virtualenv \
-	spotify \
-	neofetch \
-	btop \
-	docker \
-	ranger \
-	github-cli
+  nodejs \
+  npm \
+  python-pip \
+  python-virtualenv \
+  fastfetch \
+  neofetch \
+  btop \
+  docker \
+  github-cli
 
 echo '#######################'
 echo 'installing fonts'
 echo '#######################'
 yay -S --noconfirm --needed \
-	ttf-iosevka \
-	ttf-fira-code \
-	ttf-firacode-nerd \
-	ttf-jetbrains-mono \
-	ttf-jetbrains-mono-nerd \
-	ttf-nerd-fonts-symbols
+  ttf-iosevka \
+  ttf-fira-code \
+  ttf-firacode-nerd \
+  ttf-jetbrains-mono \
+  ttf-jetbrains-mono-nerd \
+  ttf-nerd-fonts-symbols
 
 # install docker/docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo usermod -aG docker $USER
 
-# set permissions for spicetify
-sudo chgrp users /opt/spotify
-sudo chgrp -R users /opt/spotify/Apps
-sudo chmod 775 /opt/spotify
-sudo chmod 775 -R /opt/spotify/Apps
-
 echo '#######################'
 echo 'installing additional applications'
 echo '#######################'
 yay -S --noconfirm --needed \
-	starship \
-	xclip \
-	spicetify-cli \
-	gimp \
-	vivaldi \
-	vivaldi-ffmpeg-codecs \
-	firefox-developer-edition \
-	vlc \
-	imgur-screenshot-git \
-	sxiv \
-	mpv \
-	youtube-dl \
-	mupdf \
-	surf \
-	zathura \
-	zathura-pdf-poppler
+  starship \
+  xclip \
+  gimp \
+  firefox \
+  vlc \
+  imgur-screenshot-git \
+  sxiv \
+  mpv \
+  mupdf \
+  surf
--- init.sh
+++ init.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-source ./Scripts/getDistro.sh
-machine=$(./Scripts/machine.sh)
-
 echo "Initializing dotfiles..."
-echo "Environment: $OS"
 
 # apply config
 rm -f \
-	~/.xinitrc \
-	~/.xprofile \
-	~/.Xresources \
-	~/.gitconfig
+  ~/.xinitrc \
+  ~/.xprofile \
+  ~/.Xresources \
+  ~/.gitconfig
 
 # symlink .config directory and contents to $HOME/.config
 cp -rf --symbolic-link $(pwd)/.config/* ~/.config/
@@ -26,13 +22,6 @@ ln -sf $(pwd)/.xinitrc ~/.xinitrc
 ln -sf $(pwd)/.xprofile ~/.xprofile
 ln -sf $(pwd)/.Xresources ~/.Xresources
 
-# spicetify setup
-spicetify &&
-	spicetify config current_theme catppuccin &&
-	spicetify config color_scheme macchiato &&
-	spicetify config inject_css 1 inject_theme_js 1 replace_colors 1 overwrite_assets 1 &&
-	spicetify backup apply enable-devtools
-
 ln -sf $(pwd)/.gitconfig ~/.gitconfig
 ln -sf $(pwd)/.ignore ~/.ignore
 
@@ -42,21 +31,8 @@ ln -sf $(pwd)/.ignore ~/.ignore
 ln -sf $(pwd)/.tmux.conf ~/.tmux.conf
 ln -sf $(pwd)/.tmux.conf.arch ~/.tmux.conf.local
 
-#TODO: Setup a similar note-taking setup with Google Drive
-# set up dropbox-stored note taking
-# if [ -d "$HOME/Drive" ]; then
-#   touch $HOME/Drive/note.txt
-#   ln -sf $HOME/Drive/note.txt $HOME/note.txt
-# fi
-
 # copy scripts
 sudo cp -rf --symbolic-link $(pwd)/Scripts/* /usr/local/bin/
-
-# Crontabs (only non-mac OS)
-#if [[ $OS != 'Darwin' ]];then
-#  sudo touch /var/log/cron.log
-#  sudo cp -rf --symbolic-link $(pwd)/cron/* /etc/cron.d/
-#fi
 
 # set shell as fish
 chsh -s /usr/bin/fish
@@ -64,11 +40,6 @@ chsh -s /usr/bin/fish
 # set up vim/neovim
 mkdir -p ~/.config/vim/backup_files
 mkdir -p ~/.config/vim/undo_files
-
-# install node js and utils
-# if [ ! -d "$HOME/.nvm" ]; then
-# 	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.3/install.sh | bash
-# fi
 
 # copy bin directory
 sudo cp -rf --symbolic-link $(pwd)/bin/* /usr/local/bin
